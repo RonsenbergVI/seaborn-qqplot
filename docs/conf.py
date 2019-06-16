0bc997ce730aaf19dc264a1d1c7281c146ecfaad
RonsenbergVI <corneille.r.j@gmail.com>
2019-06-16 23:37:44 +0100

    shpinx images

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages'
+    'sphinx.ext.githubpages',
+     'sphinxcontrib.images'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -4,6 +4,6 @@ SOURCE="${BASH_SOURCE[0]}"
 
 DIR="$( cd .. "$( cd -P "$( dirname "$SOURCE")" && pwd )" && pwd )"
 
-TEST_DIR=$DIR/trendpy/tests
+TEST_DIR=$DIR/seaborn_qqplot/tests
 
 python3 $TEST_DIR/main.py
