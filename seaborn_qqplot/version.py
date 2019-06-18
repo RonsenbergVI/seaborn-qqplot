f307350c9119febf3f2d80628b9d00e3513dcf6a
RonsenbergVI <corneille.r.j@gmail.com>
2019-06-18 18:41:14 +0100

    relative import of main functions

--- seaborn_qqplot/version.py
+++ seaborn_qqplot/version.py
@@ -1,8 +1,8 @@
 
 # FILE CONTENT GENERATED FROM SETUP.PY
-short_version = '0.2.4'
-version = '0.2.4'
-full_version = '0.2.4'
+short_version = '0.2.5'
+version = '0.2.5'
+full_version = '0.2.5'
 release = True
 
 if not release:
