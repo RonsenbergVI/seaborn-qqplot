b518b88f62c115cf9fe3c0d10f5fe327767e76d5
RonsenbergVI <corneille.r.j@gmail.com>
2019-06-18 18:27:50 +0100

    missing import statement in __init__.py file

--- seaborn_qqplot/__init__.py
+++ seaborn_qqplot/__init__.py
@@ -28,8 +28,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from seaborn_qqplot.plots import qqplot
 from seaborn_qqplot.version import version
 
 __version__ = version
-
-__all__ = ["qqplot", "probability_plot"]
--- seaborn_qqplot/version.py
+++ seaborn_qqplot/version.py
@@ -1,8 +1,8 @@
 
 # FILE CONTENT GENERATED FROM SETUP.PY
-short_version = '0.2.3'
-version = '0.2.3'
-full_version = '0.2.3'
+short_version = '0.2.4'
+version = '0.2.4'
+full_version = '0.2.4'
 release = True
 
 if not release:
