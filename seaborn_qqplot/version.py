a08277efa59b4d91625247541ecd95277540438c
RonsenbergVI <corneille.r.j@gmail.com>
2019-08-08 23:08:25 +0100

    added example with confidence intervals in jupyter notebook

--- seaborn_qqplot/version.py
+++ seaborn_qqplot/version.py
@@ -1,8 +1,8 @@
 
 # FILE CONTENT GENERATED FROM SETUP.PY
-short_version = '0.3.1'
-version = '0.3.1'
-full_version = '0.3.1'
+short_version = '0.3.2'
+version = '0.3.2'
+full_version = '0.3.2'
 release = True
 
 if not release:
