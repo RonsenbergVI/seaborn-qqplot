5e30b3d8ec9464c2ef64af3fb9008eca3816132f
Rene Jean Corneille <corneille.r.j@gmail.com>
2020-06-20 23:07:38 +0100

    validation functions

---
+++ seaborn_qqplot/validation.py
@@ -0,0 +1,73 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2019, Rene Jean Corneille
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from pandas import DataFrame
+
+
+def _validate_data(data, dropna=True):
+    """
+    Only supports  pandas.DataFrame as input
+    """
+    if not isinstance(data, DataFrame):
+        raise TypeError(
+            "'data' must be pandas DataFrame object, not: {typefound}".format(
+                typefound=type(data)))
+    if dropna:
+        data.copy().dropna(axis=0, inplace=True)
+    return data.copy()
+
+
+def _validate_x_and_y(x, y):
+    """
+    Validate input data.
+    """
+    if x is None or y is None:
+        raise TypeError("x and y cannot be of NoneType")
+    return x, y
+
+
+def _validate_argument(arg, argname, valid_args):
+    """
+    Validate interpolation method for quantile function.
+    """
+    if arg not in valid_args:
+        msg = 'Invalid value for {} ({}). Must be on of {}.'
+        raise ValueError(msg.format(argname, arg, valid_args))
+    return arg
+
+
+def _validate_interpolation(arg):
+    """ Validate quantile interpolation method. """
+    return _validate_argument(arg, 'interpolation', ['linear', 'lower', 'higher', 'midpoint', 'nearest'])
+
+
+def _validate_kind(arg):
+    """ Validate kind of plot. """
+    return _validate_argument(arg, 'plot kind', ['q', 'p', 'qq', 'pp'])
