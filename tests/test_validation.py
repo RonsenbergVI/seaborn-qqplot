6b44705e546713ac510e04d49b2c339ed64cad26
Rene Jean Corneille <corneille.r.j@gmail.com>
2020-07-13 00:44:22 +0100

    73% coverage

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -27,3 +27,44 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import unittest
+
+from pandas import DataFrame
+
+from seaborn_qqplot.validation import (
+    _validate_data,
+    _validate_x_and_y,
+    _validate_interpolation,
+    _validate_kind,
+    _validate_argument
+)
+
+class TestTransform(unittest.TestCase):
+
+
+    def test_validate_data_fails(self):
+        data = DataFrame()
+        self.assertEqual(_validate_data(data).values.tolist(), [])
+
+    def test_validate_data(self):
+        data = dict()
+        with self.assertRaises(TypeError):
+            _validate_data(data)
+            
+    def test_validate_x_and_y_fails(self):
+        with self.assertRaises(TypeError):
+            _validate_x_and_y(None, None)
+
+    def test_validate_interpolation_fails(self):
+        with self.assertRaises(ValueError):
+            _validate_interpolation(25)
+
+    def test_validate_kind_fails(self):
+        with self.assertRaises(ValueError):
+            _validate_kind('qqq')
+        
+    def test_validate_argument_fails(self):
+        with self.assertRaises(ValueError):
+            _validate_argument('a', 'test', ['b', 'c'])
+        
