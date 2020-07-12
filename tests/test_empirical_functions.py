9a6894a213a7ddacfbc71699e02565c2baa193f1
Rene Jean Corneille <corneille.r.j@gmail.com>
2020-07-12 22:39:31 +0100

    Update test_empirical_functions.py

--- tests/test_empirical_functions.py
+++ tests/test_empirical_functions.py
@@ -28,6 +28,8 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import unittest
+
 import numpy as np
 import numpy.testing as npt
 from numpy.testing import assert_raises
@@ -35,16 +37,17 @@ from numpy.testing import assert_raises
 from seaborn_qqplot.empirical_functions import StepFunction, monotone_fn_inverter
 
 # from seaborn_qqplot.distributions.tests
-class TestDistributions(object):
+class TestDistributions(unittest.TestCase):
 
-    def test_StepFunction(self):
+    def test_step_function(self):
         x = np.arange(20)
         y = np.arange(20)
         f = StepFunction(x, y)
         npt.assert_almost_equal(f( np.array([[3.2,4.5],[24,-3.1],[3.0, 4.0]])),
                                              [[ 3, 4], [19, 0],  [2, 3]])
 
-    def test_StepFunctionBadShape(self):
+
+    def test_step_function_bad_shape(self):
         x = np.arange(20)
         y = np.arange(21)
         assert_raises(ValueError, StepFunction, x, y)
@@ -52,20 +55,23 @@ class TestDistributions(object):
         y = np.zeros((2, 2))
         assert_raises(ValueError, StepFunction, x, y)
 
-    def test_StepFunctionValueSideRight(self):
+
+    def test_step_function_value_side_right(self):
         x = np.arange(20)
         y = np.arange(20)
         f = StepFunction(x, y, side='right')
         npt.assert_almost_equal(f( np.array([[3.2,4.5],[24,-3.1],[3.0, 4.0]])),
                                              [[ 3, 4], [19, 0],  [3, 4]])
 
-    def test_StepFunctionRepeatedValues(self):
+
+    def test_step_function_repeated_values(self):
         x = [1, 1, 2, 2, 2, 3, 3, 3, 4, 5]
         y = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
         f = StepFunction(x, y)
         npt.assert_almost_equal(f([1, 2, 3, 4, 5]), [0, 7, 10, 13, 14])
         f2 = StepFunction(x, y, side='right')
         npt.assert_almost_equal(f2([1, 2, 3, 4, 5]), [7, 10, 13, 14, 15])
+
 
     def test_monotone_fn_inverter(self):
         x = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
