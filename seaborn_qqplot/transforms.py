correct formula

seaborn_qqplot/transforms.py
@@ -71,7 +71,7 @@ class ConfidenceInterval(TransformMixin):
         df      = N - 2
         q_t     = t.ppf(self.p, df)
         s_err   = self.y.std(ddof=2)
-        c       = q_t * s_err * np.sqrt(1/N) + (x2-np.mean(self.x))**2/np.sum((self.x-np.mean(self.x))**2
+        c       = q_t * s_err * np.sqrt(1/N) + (x2-np.mean(self.x))**2/np.sum((self.x-np.mean(self.x))**2)
         return x2, y2+c, y2-c
 
 class Scale(TransformMixin):